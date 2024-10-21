35e1094279ad6aa3908916f380ff91654106a132
엄태권 <akfls221@gmail.com>
2024-10-22 02:52:47 +0900

    feat : add private final field

--- src/main/java/com/example/springbatchexample/configuration/jobrunner/JobRunner.java
+++ src/main/java/com/example/springbatchexample/configuration/jobrunner/JobRunner.java
@@ -1,9 +1,21 @@
 package com.example.springbatchexample.configuration.jobrunner;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
 public class JobRunner implements ApplicationRunner {
+
+    private final JobLauncher jobLauncher;
+    private final Job job;
+
+    public JobRunner(JobLauncher jobLauncher, @Qualifier("exampleJob3") Job job) {
+        this.jobLauncher = jobLauncher;
+        this.job = job;
+    }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
