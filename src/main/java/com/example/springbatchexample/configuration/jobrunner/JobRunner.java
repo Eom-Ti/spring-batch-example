17a88cafecb61e585d623aa7538c243256312a0e
엄태권 <akfls221@gmail.com>
2024-10-22 02:50:50 +0900

    feat : add JobRunner class

---
+++ src/main/java/com/example/springbatchexample/configuration/jobrunner/JobRunner.java
@@ -0,0 +1,12 @@
+package com.example.springbatchexample.configuration.jobrunner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class JobRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+
+    }
+}
